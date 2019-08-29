9e6b7a044d693325d7e021923c4b8820c462db33
Mrenzy123 <deborahmurenzi94@gmail.com>
2019-08-29 20:27:32 +0200

    Adding bootstrap

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,6 @@
 export class User {
+    public repoList:boolean;
+    constructor(public username: string, public profile: string, public fullName:string, public repoNumber:number ){
+        this.repoList=false;
+    }
 }
